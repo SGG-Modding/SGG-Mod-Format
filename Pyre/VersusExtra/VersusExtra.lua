e22bba39525c6bf0274fd981fec6646288f19ef4
Andre Issa <andrelouisissa@gmail.com>
2020-10-24 13:21:44 +1100

    Bench copy target config, erumi321 hiding button

--- Pyre/VersusExtra/VersusExtra.lua
+++ Pyre/VersusExtra/VersusExtra.lua
@@ -4,16 +4,19 @@ local config = {
 	TeamA = {
 		{
 			Base = 1,
+			Bench = nil,
 			NilTable = {},
 			SetTable = {},
 		},
 		{
 			Base = 2,
+			Bench = nil,
 			NilTable = {},
 			SetTable = {},
 		},
 		{
 			Base = 20,
+			Bench = nil,
 			NilTable = {},
 			SetTable = {},
 		},
@@ -21,24 +24,27 @@ local config = {
 	TeamB = {
 		{
 			Base = 8,
+			Bench = nil,
 			NilTable = {},
 			SetTable = {},
 		},
 		{
 			Base = 11,
+			Bench = nil,
 			NilTable = {},
 			SetTable = {},
 		},
 		{
 			Base = 4,
+			Bench = nil,
 			NilTable = {},
 			SetTable = {},
 		},
 	},
 }
+VersusExtra.config = config
 
 local TeamExpansion = true
-
 ModUtil.WrapBaseFunction( "PrepareLocalMPDraft", function(baseFunc, TeamAid, TeamBid )
 	if TeamExpansion then
 		local TeamAbench = League[TeamAid].TeamBench
@@ -46,20 +52,36 @@ ModUtil.WrapBaseFunction( "PrepareLocalMPDraft", function(baseFunc, TeamAid, Tea
 		local nA = #TeamAbench
 		local nB = #TeamBbench
 		for i,v in ipairs(config.TeamA) do
-			local character = DeepCopyTable(TeamAbench[v.Base])
+			local bench = TeamAbench
+			if v.Bench then bench = League[v.Bench] end
+			local character = DeepCopyTable(bench[v.Base])
 			ModUtil.MapNilTable(character,v.NilTable)
 			ModUtil.MapSetTable(character,v.SetTable)
 			character.CharacterIndex = nA+i
-			TeamAbench[nA+i] = character
+			TeamAbench[character.CharacterIndex] = character
 		end
 		for i,v in ipairs(config.TeamB) do
-			local character = DeepCopyTable(TeamBbench[v.Base])
+			local bench = TeamBbench
+			if v.Bench then bench = League[v.Bench] end
+			local character = DeepCopyTable(bench[v.Base])
 			ModUtil.MapNilTable(character,v.NilTable)
 			ModUtil.MapSetTable(character,v.SetTable)
 			character.CharacterIndex = nB+i
-			TeamBbench[nB+i] = character
+			TeamBbench[character.CharacterIndex] = character
 		end
 	end
 	TeamExpansion = false
 	return baseFunc( TeamAid, TeamBid )
+end, VersusExtra)
+
+ModUtil.WrapBaseFunction( "DisplayDraftScreen", function(baseFunc )
+    local ret = baseFunc()
+    SetMenuOptions({ Name = "RosterScreen", Item = "YButton", Properties = {OffsetY = 400} })
+    return ret
+end, VersusExtra)
+
+ModUtil.WrapBaseFunction( "ViewTeam", function(baseFunc, team )
+	local ret = baseFunc(team)
+	SetMenuOptions({ Name = "RosterScreen", Item = "YButton", Properties = {OffsetY = 400} })
+	return ret
 end, VersusExtra)
