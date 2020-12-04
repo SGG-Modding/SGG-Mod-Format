1775b0b4c614401098807b88b9455e650d8c2c49
Museus <museus@protonmail.com>
2020-12-03 19:34:31 -0500

    data: refactor functions

--- SGGMI/SGGMI/data.py
+++ SGGMI/SGGMI/data.py
@@ -1,41 +1,101 @@
 __all__ = [
-    "safeget",
-    "safeset",
-    "dictmap",
+    "get_attribute",
+    "set_attribute",
+    "merge_dict",
 ]
 
-def safeget(data, key, default=DNE, skipnone=True):
-    if data is None:
-        data = globals()
-    if isinstance(data, list) or isinstance(data, tuple):
-        if isinstance(key, int):
-            if key < len(data) and key >= 0:
-                ret = data[key]
-                return default if skipnone and ret is None else ret
-        return default
-    if isinstance(data, dict):
-        ret = data.get(key, default)
-        return default if skipnone and ret is None else ret
-    return default
+
+def get_attribute(data, key, default=None):
+    """
+    Return value at 'key' from dictionary, list, or tuple
+
+    Arguments:
+    data -- dictionary, list, or tuple to get value at 'key' from
+    key -- index or key to get from 'data'
+
+    Keyword Arguments:
+    default -- value to return if no value is found at 'key' (Default: None)
+
+    Returns:
+    Value at 'key' or provided 'default' value
+    """
+    result = default
+
+    if (
+        (isinstance(data, list) or isinstance(data, tuple))
+        and isinstance(key, int)
+        and 0 <= key < len(data)
+    ):
+        result = data[key]
+
+    if isinstance(data, dict) and key in data:
+        result = data[key]
+
+    if result is None:
+        result = default
+
+    return result
 
 
-def safeset(data, key, value):
-    if data is None:
-        data = globals()
-    if isinstance(data, list):
-        if isinstance(key, int):
-            if key < len(data) and key >= 0:
-                data[key] = value
+def set_attribute(data, key, value):
+    """
+    Set value at 'key' in a dictionary or list
+
+    Arguments:
+    data -- dictionary or list to modify
+    key -- index or key to set in 'data'
+    value -- value to store at 'key' in 'data'
+
+    Returns:
+    True if value was updated
+    """
+    if isinstance(data, list) and isinstance(key, int) and 0 <= key < len(data):
+        data[key] = value
+        return True
+
     if isinstance(data, dict):
         data[key] = value
+        return True
+
+    return False
 
 
-def dictmap(indict, mapdict):
-    if mapdict is DNE or mapdict is indict:
-        return indict
-    if type(indict) == type(mapdict):
-        if isinstance(mapdict, dict):
-            for k, v in mapdict.items():
-                indict[k] = dictmap(safeget(indict, k), v)
-            return indict
-    return mapdict
+def merge_dict(base_dict, input_dict, modify_original=True):
+    """
+    Merge input_dict into base_dict, overwriting entries in base_dict as needed.
+
+    Arguments:
+    base_dict -- dictionary to start with
+    input_dict -- dictionary to merge into base_dict
+
+    Keyword Arguments:
+    modify_original -- If False, original base_dict is left intact (Default: True)
+
+    Returns:
+    Resulting dictionary after merging
+    """
+    # Check if anything needs to be done first
+    if not base_dict:
+        return input_dict
+
+    if not input_dict:
+        return base_dict
+
+    # Create new dict if modify_original is False
+    target_dict = dict(base_dict) if not modify_original else base_dict
+
+    for key in input_dict:
+        if key not in target_dict:
+            target_dict[key] = input_dict[key]
+            continue
+
+        # If both are dictionaries, need to merge recursively
+        if isinstance(target_dict[key], dict) and isinstance(input_dict[key], dict):
+            merge_dict(target_dict[key], input_dict[key])
+        # If they're the same, no need to update
+        elif target_dict[key] == input_dict[key]:
+            pass  # same leaf value
+        else:
+            target_dict[key] = input_dict[key]
+
+    return target_dict
