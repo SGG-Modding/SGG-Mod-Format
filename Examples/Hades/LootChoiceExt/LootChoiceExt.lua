ba7982d73d050c367533672fdbc3d662db890a33
Andre Issa <andrelouisissa@gmail.com>
2021-09-04 04:04:46 +1000

    move from ipairs to CreateScreenComponent

--- Examples/Hades/LootChoiceExt/LootChoiceExt.lua
+++ Examples/Hades/LootChoiceExt/LootChoiceExt.lua
@@ -31,66 +31,20 @@ end, LootChoiceExt)
 ModUtil.Path.Context.Wrap("CreateBoonLootButtons", function( )
 	local data = { }
 	local active = false
-	local first = true
-
-	ModUtil.Path.Wrap("ipairs", function( base, tbl )
-		if not active then
-			local locals = ModUtil.Locals.Stacked( )
-			if tbl == locals.upgradeOptions then
-				active = true
-				
-				data.upgrade = locals.upgradeData
-				data.blocked = locals.blockedIndexes
-				data.options = tbl
-				data.length = #tbl
-				local excess = math.max( 3,data.length )-3
-				locals.itemLocationY = locals.itemLocationY-12.5*excess
-				data.squashY = 3/(3+excess)
-				data.iconScaling = (excess == 0) and 0.85 or 1
-			end
-		end
-		return base( tbl )
-	end, LootChoiceExt )
-	
-	ModUtil.Path.Wrap( "SetScale", function( base, args )
-		if active then
-			args.Fraction = data.iconScaling
-			SetScaleY(args)
-			args.Fraction = data.iconScaling*data.squashY
-			return SetScaleX(args)
-		end
-		return base( args )
-	end, LootChoiceExt)
-
-	ModUtil.Path.Wrap( "SetScaleY", function( base, args )
-		if active then
-			args.Fraction = args.Fraction*data.squashY
-		end
-		return base( args )
-	end, LootChoiceExt)
-	
-	ModUtil.Path.Wrap( "CreateTextBox", function( base, args )
-		if active then
-			if args.OffsetY then
-				args.OffsetY = args.OffsetY*data.squashY
-			end
-			if data.upgrade and args.Text == data.upgrade.CustomRarityName then 
-				ModUtil.Locals.Stacked( ).lineSpacing = 8*data.squashY
-			end
-		end
-		return base( args )
-	end, LootChoiceExt )
-	
 	ModUtil.Path.Wrap( "CreateScreenComponent", function( base, args )
-		if active and args.Group == "Combat_Menu" then
+		if not active and args.Name == "TraitBacking" then
+			active = true 
+			local locals = ModUtil.Locals.Stacked( )
+			data.upgrade = locals.upgradeData
+			local excess = math.max( 3,#locals.upgradeOptions )-3
+			locals.itemLocationY = locals.itemLocationY-12.5*excess
+			data.squashY = 3/(3+excess)
+			data.iconScaling = (excess == 0) and 0.85 or 1
+		elseif active and args.Group == "Combat_Menu" then
 			local locals = ModUtil.Locals.Stacked( )
 			if args.Name == "TraitBacking" then
-				if first then
-					first = false
-				else
-					locals.itemLocationY = locals.itemLocationY + 220*(data.squashY-1)
-					args.Y = locals.itemLocationY
-				end
+				locals.itemLocationY = locals.itemLocationY + 220*(data.squashY-1)
+				args.Y = locals.itemLocationY
 			end
 			if args.Name == "BoonSlot"..locals.itemIndex then
 				locals.iconOffsetY = -2*data.squashY
@@ -101,14 +55,32 @@ ModUtil.Path.Context.Wrap("CreateBoonLootButtons", function( )
 		SetScaleY({Id = component.Id, Fraction = 1})
 		return component
 	end, LootChoiceExt )
-	
-	ModUtil.Path.Wrap("IsMetaUpgradeSelected", function( base, arg )
-		if active and arg == "RerollPanelMetaUpgrade" then
-			active = false
+	ModUtil.Path.Wrap( "CreateTextBox", function( base, args )
+		if active then
+			if args.OffsetY then args.OffsetY = args.OffsetY*data.squashY end
+			if data.upgrade and args.Text == data.upgrade.CustomRarityName then 
+				ModUtil.Locals.Stacked( ).lineSpacing = 8*data.squashY
+			end
 		end
+		return base( args )
+	end, LootChoiceExt )
+	ModUtil.Path.Wrap( "SetScale", function( base, args )
+		if active then
+			args.Fraction = data.iconScaling
+			SetScaleY(args)
+			args.Fraction = data.iconScaling*data.squashY
+			return SetScaleX(args)
+		end
+		return base( args )
+	end, LootChoiceExt)
+	ModUtil.Path.Wrap( "SetScaleY", function( base, args )
+		if active then args.Fraction = args.Fraction*data.squashY end
+		return base( args )
+	end, LootChoiceExt)
+	ModUtil.Path.Wrap( "IsMetaUpgradeSelected", function( base, arg )
+		if active and arg == "RerollPanelMetaUpgrade" then active = false end
 		return base( arg )
 	end, LootChoiceExt )
-	
 end, LootChoiceExt)
 
 ModUtil.Path.Override("DestroyBoonLootButtons", function( lootData )
